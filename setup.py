e5c297fd5d0f63ebb3abcce94537e35944cab1e3
Daniel S. Koda <danielskoda@gmail.com>
2016-03-03 22:49:13 -0300

    Install script added

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+ 
+    
+setup (
+	name='vaspirin',
+	packages=['vaspirin'],
+	entry_points = {
+        "console_scripts": ['vaspirin = vaspirin.vaspirin:main']
+        },
+	version='1.0',
+	description='Painless VASP post-processing tool',
+	author='Daniel S. Koda, Ivan Guilhon',
+	author_email='danielskoda@gmail.com, ivanguilhonn@gmail.com ',
+	url='http://www.gmsn.ita.br/',
+	)
